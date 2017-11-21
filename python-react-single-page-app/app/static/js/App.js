da6fd1d7253d30d44f27c29bb46fe49bb2c2d53c
Jessica O'Brien <j.e.obrien28@gmail.com>
2017-11-20 17:45:07 -0800

    Add python single page app example

---
+++ python-react-single-page-app/app/static/js/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Intercom Messenger installation example</h1>
+        <p>You should see the messenger in the bottom right if you set it up correctly!</p>
+
+        <blockquote cite="http://mentalfloss.com/article/56004/16-fun-facts-about-hedgehogs">
+          <p><strong>Did you know?</strong></p>
+          <p>A group of hedgehogs is called an array.</p>
+        </blockquote>
+      </div>
+    )
+  }
+}
